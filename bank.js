a09e407925e6017dbb484dd535bdd834655eaf0c
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-23 15:47:11 +0000

    Used jQuery to update view and accept params

--- bank.js
+++ bank.js
@@ -8,15 +8,27 @@ $(document).ready(function(){
     $('#show-balance').text(bankAccount.showBalance())
   }
 
-  function getStatement(){
-    $('#get-statement').on('click', function(event){
-      event.preventDefault();
-      $('#show-statement').text(bankAccount.statement.printContents())
-    })
-  }
 
-  function addTxn(){
-    $()
-  }
+  $('#get-statement').on('click', function(event){
+    event.preventDefault();
+    $('#show-statement').text(bankAccount.statement.printStatement())
+  })
+
+  $('#transaction').on('submit', function(event){
+    event.preventDefault();
+    var inputs = $(this).serializeArray()
+    var type = inputs[0].value
+    var value = parseInt(inputs[1].value)
+
+    if (type == "Credit"){
+      bankAccount.deposit(value)
+    }
+    else {
+      bankAccount.withdraw(value)
+    }
+    updateView()
+  })
+
+  //type=Credit&value=22
 
 })
