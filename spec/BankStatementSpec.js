a1825555bacee2eebeca5a32fd1055de8bcfac4e
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-23 14:29:52 +0000

    added methods to generate statement

--- spec/BankStatementSpec.js
+++ spec/BankStatementSpec.js
@@ -4,6 +4,21 @@ describe("Bank Statement", function(){
 
   beforeEach(function(){
     bankStatement = new BankStatement()
+    TransactionDouble = function(){
+      this.date = "23/01/2017"
+      this.debit = true
+      this.credit = false
+      this.balance = 20.00
+      this.value = 4.00
+    }
+
+    TransactionDouble2 = function(){
+      this.date = "22/01/2017"
+      this.debit = false
+      this.credit = true
+      this.balance = 32.00
+      this.value = 6.00
+    }
   })
 
   it("can store transactions", function(){
@@ -16,6 +31,28 @@ describe("Bank Statement", function(){
     expect(bankStatement.transactions).toEqual(jasmine.arrayContaining([transactionDouble]))
   })
 
+  it("can print header of statement", function(){
+    header = "Date       ||Credit    ||Debit    ||Balance   \n"
+    expect(bankStatement.printHeader()).toEqual(header)
+  })
+
+  it("can print contents of statements", function(){
+    var testTransaction = new TransactionDouble()
+    var testTransaction2 = new TransactionDouble2()
+    bankStatement.addTransaction(testTransaction)
+    bankStatement.addTransaction(testTransaction2)
+    content = "23/01/2017 ||          ||4.00      ||20.00     \n"
+    content2 = "22/01/2017 ||6.00      ||          ||32.00     \n"
+    expect(bankStatement.printContents()).toEqual(content + content2)
+  })
+
+  it("can print out full statement", function(){
+    var testTransaction = new TransactionDouble()
+    bankStatement.addTransaction(testTransaction)
+    header = "Date       ||Credit    ||Debit    ||Balance   \n"
+    content = "23/01/2017 ||          ||4.00      ||20.00     \n"
+    expect(bankStatement.printStatement()).toEqual(header + content)
+  })
 
 
 })
--- src/BankStatement.js
+++ src/BankStatement.js
@@ -8,6 +8,30 @@
     this.transactions.push(transaction)
   }
 
+  BankStatement.prototype.printContents = function(){
+    var output = ""
+    this.transactions.forEach(function(transaction){
+      output += transaction.date + " ||"
+      console.log(typeof(transaction.date))
+      if (transaction.credit === true){
+        output += transaction.value.toFixed(2) + "       ||          ||"
+      }
+      else if (transaction.debit === true){
+        output += "          ||" + transaction.value.toFixed(2) + "      ||"
+      }
+      output += transaction.balance.toFixed(2) + "     \n"
+    })
+    return output
+  }
+
+  BankStatement.prototype.printHeader = function(){
+    return "Date       ||Credit    ||Debit    ||Balance   \n"
+  }
+
+  BankStatement.prototype.printStatement = function(){
+    return this.printHeader() + this.printContents()
+  }
+
   exports.BankStatement = BankStatement
-  
+
 })(this);
