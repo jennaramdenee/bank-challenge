c32e6fc6e2770f910c6781ef9f1ab0051c9e2269
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-23 09:50:12 +0000

    Removed unnecessary files

--- src/Airport.js
+++
@@ -1,9 +0,0 @@
-var Airport = function(){this.planes = []};
-
-Airport.prototype.receiveLandedPlane = function(plane){
-  this.planes.push(plane);
-};
-
-Airport.prototype.confirmLanding = function(plane){
-  return this.planes.includes(plane);
-};
